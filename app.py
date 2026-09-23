35abc1322004584ff99db8b966334e4ff2684247
rushikeshlone13-commits <rushikeshlone13@gmail.com>
2026-09-23 22:39:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -366,7 +366,7 @@ else:
                         )
                     except Exception:
                         response = client.models.generate_content(
-                            model='gemini-1.5-flash-8b',
+                            model='gemini-1.5-pro',
                             contents=processed_contents
                         )
 
